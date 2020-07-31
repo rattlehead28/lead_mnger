52107905ff34d8792dacb3c63b10f98b78902bdb
Kartikeya Mudgal <kartikeya.mudgal28@gmail.com>
2020-07-31 15:52:18 +0530

    added spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,59 @@
-import { Component, HostListener, Renderer2 } from '@angular/core';
-import { environment } from 'src/environments/environment';
+import { Component, HostListener, Renderer2, ViewChild, ElementRef } from "@angular/core";
+import { environment } from "src/environments/environment";
+import { Router, Event, NavigationStart, NavigationEnd } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-
-  title = 'lead-mngr';
-  @HostListener('window:resize',['$event'])
-  addClassOnResize(evt){
-  this.handleClass();
+  title = "lead-mngr";
+  showLoader = true;
+  @ViewChild('loaderRef',{static:false}) loaderRef: ElementRef;
+  @HostListener("window:resize", ["$event"])
+  addClassOnResize(evt) {
+    this.handleClass();
   }
 
-  constructor(private renderer: Renderer2) {
+  constructor(private renderer: Renderer2, private router: Router) {
     console.log(environment.apiUrl);
   }
 
-  ngOnInit(){
-   this.handleClass();
+  ngOnInit() {
+    console.log(window)
+    this.router.events.subscribe( (event: Event) => {
+
+      if (event instanceof NavigationStart) {
+          // Show loading indicator
+          console.log('start')
+      }
+
+      if (event instanceof NavigationEnd) {
+          // Hide loading indicator
+          console.log('end')
+      }
+
+  });
+
+
+    this.handleClass();
   }
 
-  handleClass(){
-    const windowSize = window.innerWidth;
-    console.log(windowSize)
-    if(windowSize<=900){
-      this.renderer.addClass(document.body, 'isPlatformMobile');
+  ngAfterViewInit(){
+    if(window.innerWidth){
+      this.renderer.setStyle(this.loaderRef.nativeElement,'visibility','hidden');
     }
-    else{
-      this.renderer.removeClass(document.body, 'isPlatformMobile');
+    
+  }
+
+  handleClass() {
+    const windowSize = window.innerWidth;
+    console.log(windowSize);
+    if (windowSize <= 900) {
+      this.renderer.addClass(document.body, "isPlatformMobile");
+    } else {
+      this.renderer.removeClass(document.body, "isPlatformMobile");
     }
   }
 }
